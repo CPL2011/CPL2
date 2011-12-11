58837db06da1422ab775a22081aa092443f13b81
Van Daele D <dandonio@hotmail.com>
2011-12-11 19:32:47 +0100

    some supporting code, starting out by providing an 'easy' way to handle Flights, dates,...

---
+++ Flight.rb
@@ -0,0 +1,72 @@
+class Flight
+  attr_reader :airportCodes
+
+  # airports : An array of airport codes. 
+  # The first element is the code of the departure airport, 
+  # the last one is the code of the destination airport,
+  # everything in between are the stops made in their respective sequence.
+  def initialize(airports)
+    @airportCodes = airports
+  end
+  
+  # Checks the validity of this Flight.
+  # A flight is valid when it's physically possible for the passenger
+  # to 'catch his/her next flight'.
+  def isValid
+    
+  end
+  
+  # returns the total estimated time the traveller will be airbone 
+  # (excluding waiting times in between flights)
+  def totalFlightTime
+
+  end
+  
+  # returns the total estimated time passed between the departure and 
+  # the moment the destination is reached
+  def totalTripTime
+
+  end  
+end
+#-------------------------------------------------------------------------------
+
+class Date
+  attr_reader :date
+
+  def initialize(date, time)
+    raise "Missing date" if date.nil?
+    splitData = date.split(/-/)
+    if (time.nil?) 
+      @date = Time.new(splitData[0], splitData[1], splitData[2], 0, 0, 0)
+    else 
+      timeWrap = MyTime.new(time)
+      @date = Time.new(splitData[0], splitData[1], splitData[2], timeWrap.seconds/3600, timeWrap.seconds%3600/60)  
+    end
+  end
+  
+  def addTimeToDate(addTime)
+    time = MyTime.new(addTime)
+    return @date + time.seconds
+  end
+
+  def compare(date, time)
+    return date <=> Date.new(date, time).date
+  end
+end
+
+class MyTime
+  attr_reader :seconds
+  def initialize(time)
+    splitTime = time.split(/:/)
+    hours = splitTime[0]
+    minutes = splitTime[1]
+    @seconds = hours.to_i*3600 + minutes.to_i*60 
+  end
+end
+
+
+test1 = Date.new("2011-03-12", "10:30")
+test2 = Date.new("2011-03-12", nil)
+puts test1.date
+puts test2.date
+puts test1.addTimeToDate("17:55")
