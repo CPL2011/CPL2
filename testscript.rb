5cdfb84b5c17428f75aadf499090edf6bc08c09e
Jordi Tielens <jordi.tielens@student.kuleuven.be>
2011-12-16 23:35:36 +0100

    test script continued

--- testscript.rb
+++ testscript.rb
@@ -11,12 +11,23 @@ puts $adameus.airlines
 puts "\nadameus.airports"
 puts $adameus.airports
 
+puts "\nadameus.destinations(\"BRU\")"
+puts $adameus.destinations("BRU")
+
 puts "\nadameus.connections(\"VIE\", \"BRU\", \"2012-01-15\")"
 puts $adameus.connections("VIE", "BRU", "2012-01-15")
 
-puts "\nadameus.weekdays(\"Whrong FlightNumber\")"
-$adameus.weekdays("Whrong FlightNumber")
+puts "\nflight_airports(\"BEL062\")"
+puts $adameus.flight_airports("BEL062")
 
+puts "\nadameus.weekdays(\"Wrong FlightNumber\")"
+puts $adameus.weekdays("Wrong FlightNumber")
+
+puts "\nadameus.weekdays(\"SJT208\")"
+puts $adameus.weekdays("SJT208")
+
+puts "\nadameus.seats(\"2012-01-15\", \"SJT208\", \"B\")"
+puts $adameus.seats("2012-01-15", "SJT208", "B")
 #puts "\nadameus.hold_cheapest(\"2012-01-15\", \"TEG\", \"AKL\",  \"B\", \"M, Edsger, Dijkstra\", \"M, John, McCarthy\")"
 #puts $adameus.hold_cheapest("2012-01-15", "TEG", "AKL",  "B", "M, Edsger, Dijkstra", "M, John, McCarthy")
 #puts $adameus.book("tralalalalal")
